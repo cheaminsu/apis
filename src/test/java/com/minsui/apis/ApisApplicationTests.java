431c10971f606957224296cc7fda788fc64c5449
cheaminsu <71109715+cheaminsu@users.noreply.github.com>
2020-12-14 13:12:08 +0900

    Initial commit

---
+++ src/test/java/com/minsui/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsui.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
