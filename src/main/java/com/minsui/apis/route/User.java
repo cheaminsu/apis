4992b81f265d925bfd2a4162f27ecd7d3964ebc9
cheaminsu <71109715+cheaminsu@users.noreply.github.com>
2020-12-14 14:12:53 +0900

    add: route 추가한다

--- src/main/java/com/minsui/apis/route/User.java
+++ src/main/java/com/minsui/apis/route/User.java
@@ -1,4 +1,12 @@
 package com.minsui.apis.route;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class User {
+
+    @GetMapping("/user")
+    public String getUsers(){
+        return  "users!";
+    }
 }
